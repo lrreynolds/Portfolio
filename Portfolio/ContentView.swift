4435e3e42569da054ac4a6a1118046e639c3a240
Lou Reynolds <lou@peakx.com>
2020-12-05 15:50:02 -0700

    Initial Commit

---
+++ Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by LouR on 12/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by LouR on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
